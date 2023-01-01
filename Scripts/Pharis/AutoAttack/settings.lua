1f86dd2e8272b88824a8f0b586d4c1dd41e8c846
PharisMods <rmassey3234@gmail.com>
2023-01-01 16:28:18 -0600

    Shorten settings keys

--- Scripts/Pharis/AutoAttack/settings.lua
+++ Scripts/Pharis/AutoAttack/settings.lua
@@ -18,14 +18,15 @@ local modName = modInfo.modName
 local modVersion = modInfo.modVersion
 
 -- General settings description(s)
-local modEnableConfDesc = "To mod or not to mod."
-local logDebugConfDesc = "Press F10 to see logged messages in-game."
-local modHotkeyConfDesc = "Choose which key toggles auto attack."
-local attackTimerIntervalConfDesc = "Auto attack timer interval in seconds. Very low or high values will obviously be rather impractical."
-local showMessagesConfDesc = "Show messages on screen when auto attack is toggled."
-local stopOnReleaseConfDesc = "Stops auto attacking when the hotkey is released."
-local attackBindingModeConfDesc = "Binds auto attack to the attack button assigned in the controls menu (typically left click). Overrides hotkey setting."
-local marksmanOnlyConfDesc = "Limits auto attack to marksman weapons only. Implemented mostly in case it is useful for mods such as Starwind."
+local modEnableDescription = "To mod or not to mod."
+local logDebugDescription = "Press F10 to see logged messages in-game."
+local modHotkeyDescription = "Choose which key toggles auto attack."
+local attackTimerIntervalDescription = "Auto attack timer interval in seconds. Very low or high values will obviously be rather impractical."
+local showMessagesDescription = "Show messages on screen when auto attack is toggled."
+local stopOnReleaseDescription = "Stops auto attacking when the hotkey is released."
+local attackBindingModeDescription = "Binds auto attack to the attack button assigned in the controls menu (typically left click). Overrides hotkey setting."
+local marksmanOnlyDescription = "Limits auto attack to marksman weapons only. Implemented mostly in case it is useful for mods such as Starwind."
+local useWhitelistDescription = "Allow auto attacking only for weapons on the whitelist. To add to the whitelist edit the provided 'weaponWhitelist.lua' file."
 
 -- Other variables
 local playerSettings = storage.playerSection('SettingsPlayer' .. modName)
@@ -41,12 +42,26 @@ local function setting(key, renderer, argument, name, description, default)
 	}
 end
 
+local function updateModDisabled()
+    local disabled = not playerSettings:get('modEnable')
+    I.Settings.updateRendererArgument('SettingsPlayer' .. modName, 'showDebug', {disabled = disabled})
+    I.Settings.updateRendererArgument('SettingsPlayer' .. modName, 'modHotkey', {disabled = disabled})
+    I.Settings.updateRendererArgument('SettingsPlayer' .. modName, 'stopOnRelease', {disabled = disabled})
+	I.Settings.updateRendererArgument('SettingsPlayer' .. modName, 'attackBindingMode', {disabled = disabled})
+    I.Settings.updateRendererArgument('SettingsPlayer' .. modName, 'attackTimerInterval', {disabled = disabled})
+	I.Settings.updateRendererArgument('SettingsPlayer' .. modName, 'showMessages', {disabled = disabled})
+	I.Settings.updateRendererArgument('SettingsPlayer' .. modName, 'marksmanOnlyMode', {disabled = disabled})
+	I.Settings.updateRendererArgument('SettingsPlayer' .. modName, 'useWhitelist', {disabled = disabled})
+end
+
+playerSettings:subscribe(async:callback(updateModDisabled))
+
 local function initSettings()
 	I.Settings.registerRenderer('inputKeySelection', function(value, set)
 		local name = "No Key Set"
 		if value then
 			if value == input.KEY.Escape then
-				name = input.getKeyName(playerSettings:get('modHotkeyConf'))
+				name = input.getKeyName(playerSettings:get('modHotkey'))
 			else
 				name = input.getKeyName(value)
 			end
@@ -89,14 +104,15 @@ end)
 		name = "General Settings",
 		permanentStorage = false,
 		settings = {
-			setting('modEnableConf', 'checkbox', {}, "Enable Mod", modEnableConfDesc, true),
-			setting('showDebugConf', 'checkbox', {}, "Log Debug Messages", logDebugConfDesc, false),
-			setting('modHotkeyConf', 'inputKeySelection', {}, "Auto Attack Hotkey", modHotkeyConfDesc, input.KEY.G),
-			setting('attackTimerIntervalConf', 'number', {}, "Auto Attack Timer Interval", attackTimerIntervalConfDesc, 1),
-			setting('showMessagesConf', 'checkbox', {}, "Show Messages", showMessagesConfDesc, false),
-			setting('stopOnReleaseConf', 'checkbox', {}, "Stop On Release", stopOnReleaseConfDesc, false),
-			setting('attackBindingModeConf', 'checkbox', {}, "Attack Binding Mode", attackBindingModeConfDesc, false),
-			setting('marksmanOnlyModeConf', 'checkbox', {}, "Marksman Only Mode (Starwind Mode)", marksmanOnlyConfDesc, false),
+			setting('modEnable', 'checkbox', {}, "Enable Mod", modEnableDescription, true),
+			setting('showDebug', 'checkbox', {}, "Log Debug Messages", logDebugDescription, false),
+			setting('modHotkey', 'inputKeySelection', {}, "Auto Attack Hotkey", modHotkeyDescription, input.KEY.G),
+			setting('stopOnRelease', 'checkbox', {}, "Stop On Release", stopOnReleaseDescription, false),
+			setting('attackBindingMode', 'checkbox', {}, "Attack Binding Mode", attackBindingModeDescription, false),
+			setting('attackTimerInterval', 'number', {}, "Auto Attack Timer Interval", attackTimerIntervalDescription, 1),
+			setting('showMessages', 'checkbox', {}, "Show Messages", showMessagesDescription, false),
+			setting('marksmanOnlyMode', 'checkbox', {}, "Marksman Only Mode (Starwind Mode)", marksmanOnlyDescription, false),
+			setting('useWhitelist', 'checkbox', {}, "Use Whitelist", useWhitelistDescription, false),
 		}
 	}
 
